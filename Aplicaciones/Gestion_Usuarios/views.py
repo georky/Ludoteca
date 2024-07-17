7cab9462ff264ddafef5bba7108360cba7e84c21
Georky Condoy <georky27condoy@gmail.com>
2024-07-16 21:51:12 -0500

    api wha

--- Aplicaciones/Gestion_Usuarios/views.py
+++ Aplicaciones/Gestion_Usuarios/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
 from .models import Usuario
 from django.contrib import messages
-
+import http.client
+import request2
+import json
+import jsonify
 # Create your views here.
 
 
@@ -48,18 +51,72 @@ def eliminarUsuarios(request, telefono):
 
     messages.success(request, '¡Curso eliminado!')
     return redirect('/')
+#Token de verificacion para la configuracion
+TOKEN_ANDERCODE = "ANDERCODE"
 
-#def enviarNotifi(request, telefono, nombreC, nombreR):
-    #curso = Curso.objects.get(codigo=codigo)
-    #curso.delete()
-    #mensaje= 'Estimado '+nombreR+' esta a 5 minutos que se le termine el tiempo de '+nombreC
-    #w.sendwhatmsg('+593'+telefono, mensaje, 23, 19,  15, True, 5)
+#@app.route('/webhook', methods=['GET','POST'])
+def webhook():
+    if request2.method == 'GET':
+        challenge = verificar_token(request2)
+        return challenge
+    elif request2.method == 'POST':
+        reponse = recibir_mensajes(request2)
+        return reponse
+
+def verificar_token(req):
+    token = req.args.get('hub.verify_token')
+    challenge = req.args.get('hub.challenge')
+
+    if challenge and token == TOKEN_ANDERCODE:
+        return challenge
+    else:
+        return jsonify({'error':'Token Invalido'}),401
+def enviarNotifi(texto, telefono, nombreC, nombreR):
+    #texto = texto.lower()
     
-  
+    mensaje= 'Estimado '+nombreR+' esta a 5 minutos que se le termine el tiempo de '+nombreC
+    
+    data={
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": '593979418199',
+            "type": "text",
+            "text": {
+                "preview_url": False,
+                "body": "🚀 Hola, ¿Cómo estás? Bienvenido."
+            }
+    }
+  #Convertir el diccionaria a formato JSON
+    data=json.dumps(data)
+
+    headers = {
+        "Content-Type" : "application/json",
+        "Authorization" : "Bearer EAAOyRshDbC8BOZCUrC5usRMvAQflWRerrLz2pxYaREaO2udlYsa4tfMZBChlkl1W6ZAMEklnY50bDt40YSQYCWhZByr7x6JzCOsrILZBqw0Rw20DfpbIIphjXptXF1iUAWbRBpZBxZCZC7iel2VYXVvdIxbCcZA5IUJVn001xkAbGRSgpZBhYAVHTdvVFiGNeGXdZCQ"
+    }
+
+    connection = http.client.HTTPSConnection("graph.facebook.com")
+
+    try:
+        connection.request("POST","/v19.0/346378921896150/messages", data, headers)
+        response = connection.getresponse()
+        print(response.status, response.reason)
+    except Exception as e:
+        print(json.dumps(e))
+    finally:
+        connection.close()
     
 # Create your views here.
+def recibir_mensajes(req):
+    try:
+        req = request2.get_json()
+        entry =req['entry'][0]
+        changes = entry['changes'][0]
+        value = changes['value']
+        objeto_mensaje = value['messages']
 
-
+      #return jsonify({'message':'EVENT_RECEIVED'})
+    except Exception as e:
+      return jsonify({'message':'EVENT_RECEIVED'})
 
 
 
